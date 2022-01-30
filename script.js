ec6023c2c1178dd4c743952b8b2b4d6764078354
jeremyrrose <jeremy.r.rose@gmail.com>
2022-01-30 11:33:53 -0500

    added toggle

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 const heading = document.querySelector('h1')
-heading.addEventListener('click', () => heading.classList = ["green"])
+heading.addEventListener('click', () => {
+    heading.classList.toggle("green")
+    heading.classList.toggle("blue")
+})
